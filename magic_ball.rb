6d65b56e36296a6ccfa3768aacd01a1765b1ea09
Alex_G <occkart@yahoo.com>
2019-06-25 22:53:41 +0600

    w/out extrim & changed readme & changed main

--- magic_ball.rb
+++ magic_ball.rb
@@ -1,8 +1,9 @@
 # encoding: utf-8
 # Задача 23-4 — Волшебный шар в github
 
-greeting = File.readlines(File.dirname(__FILE__) + "/data/greetings.txt")
-print greeting.sample.chomp
+greeting = File.new("./data/greetings.txt", "r:UTF-8").readlines(chomp: true).sample
+forecast = File.new("./data/forecasts.txt", "r:UTF-8").readlines(chomp: true).sample
+print greeting
 
 3.times do
   sleep 0.5
@@ -12,6 +13,4 @@ end
 sleep 0.7
 print ": "
 
-forecast = File.readlines(File.dirname(__FILE__) + "/data/forecasts.txt")
-puts forecast.sample.chomp
-sleep 3
+puts forecast
