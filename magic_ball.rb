220d43e76dc42ae82ca03acb91a944ce30b6737e
Alex_G <occkart@yahoo.com>
2019-06-25 23:26:08 +0600

    brushed

--- magic_ball.rb
+++ magic_ball.rb
@@ -1,8 +1,14 @@
 # encoding: utf-8
 # Задача 23-4 — Волшебный шар в github
 
-greeting = File.new("./data/greetings.txt", "r:UTF-8").readlines(chomp: true).sample
-forecast = File.new("./data/forecasts.txt", "r:UTF-8").readlines(chomp: true).sample
+f = File.new("./data/greetings.txt", "r:UTF-8")
+greeting = f.readlines(chomp: true).sample
+f.close
+
+f = File.new("./data/forecasts.txt", "r:UTF-8")
+forecast = f.readlines(chomp: true).sample
+f.close
+
 print greeting
 
 3.times do
